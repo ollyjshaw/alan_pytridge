afd3ccd7599195481e6bb742ef7dd22ed40e6f10
Olly Shaw <ollyjshaw@gmail.com>
2018-04-16 07:37:30 +0100

    mines a pint

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -3,10 +3,7 @@
 
 import pytest
 from alan_pytridge.skeleton import fib
-
-__author__ = "Olly Shaw"
-__copyright__ = "Olly Shaw"
-__license__ = "mit"
+from alan_pytridge.skeleton import Alan
 
 
 def test_fib():
@@ -15,3 +12,29 @@ def test_fib():
     assert fib(7) == 13
     with pytest.raises(AssertionError):
         fib(-10)
+
+def test_nothing_found_in_empty_list():
+    words = []
+    phrase = Alan().getPhrase(words)
+    assert phrase == "Lynn, I've pierced my foot on a spike!!"
+
+def test_nothing_found_in_list_with_no_partridge_words():
+    words = ["foo", "bar", "baz"]
+    phrase = Alan().getPhrase(words)
+    assert phrase == "Lynn, I've pierced my foot on a spike!!"
+
+def test_finds_a_single_partridge_words():
+    words = ["foo", "Dan", "baz"]
+    phrase = Alan().getPhrase(words)
+    assert phrase == "Mine's a Pint!"
+
+
+def test_finds_multiple_partridge_words():
+    words = ["foo", "Dan", "Lynn", "baz"]
+    phrase = Alan().getPhrase(words)
+    assert phrase == "Mine's a Pint!!"
+
+def test_finds_multiple_partridge_words2():
+    words = ["Dan", "foo", "Dan", "Lynn", "baz"]
+    phrase = Alan().getPhrase(words)
+    assert phrase == "Mine's a Pint!!!"
