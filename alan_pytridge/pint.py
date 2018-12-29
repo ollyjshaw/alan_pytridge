08aadf51f8dc83616719e7ed351e85dcda947ad9
Olly Shaw <ollyjshaw@gmail.com>
2018-12-29 14:39:33 +0000

    typos and spelling

--- alan_pytridge/pint.py
+++ alan_pytridge/pint.py
@@ -1,17 +1,14 @@
 class Alan:
     @staticmethod
-    def pwords():
+    def partridge_words():
         return ["Dan", "Lynn"]
 
     def getPhrase(self, words):
-        wordsFound = [words.count(i) for i in Alan.pwords()]
-        exx = [ "!"*words.count(i) for i in Alan.pwords()]
+        wordsFound = [words.count(i) for i in Alan.partridge_words()]
+        exx = [ "!"*words.count(i) for i in Alan.partridge_words()]
         print(wordsFound)
         if sum(wordsFound) != 0:
             exclaim = "!" * sum(wordsFound)
             return "Mine's a Pint" + exclaim
         else:
             return "Lynn, I've pierced my foot on a spike!!"
-
-    def somethingd():
-        print("yo")
