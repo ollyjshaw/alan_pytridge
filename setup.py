7c7b1b3288fd77683a45de438e6d51704fc63388
Olly Shaw <ollyjshaw@gmail.com>
2019-01-03 08:02:28 +0000

    update via  hoefling's answer

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="alan_pytridge", packages=find_packages())
+setup(name="alan_pytridge", packages=find_packages(where='src'), package_dir={'': 'src'})
--- src/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="alan_pytridge", packages=find_packages())
