39b4181419990e912c463e21893a5129950e470a
Olly Shaw <ollyjshaw@gmail.com>
2019-09-16 09:48:09 +0100

    Tweak setup

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="alan_pytridge", packages=find_packages(where='src'), package_dir={'': 'src'})
+setup(
+    name="alan_pytridge",
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    package_dir={'': 'src'},
+)
