31670da7f6506f7e1f81955c25deefc1313b0c10
Olly Shaw <ollyjshaw@gmail.com>
2019-09-16 09:30:53 +0100

    More pythonic

--- src/alan_pytridge/pint.py
+++ src/alan_pytridge/pint.py
@@ -1,12 +1,11 @@
-class Alan:
-    @staticmethod
-    def partridge_words():
-        return ["Dan", "Lynn"]
+def getPhrase(words):
+    wordsFound = [words.count(i) for i in _partridge_words()]
+    if sum(wordsFound) != 0:
+        exclaim = "!" * sum(wordsFound)
+        return "Mine's a Pint" + exclaim
+    else:
+        return "Lynn, I've pierced my foot on a spike!!"
 
-    def getPhrase(self, words):
-        wordsFound = [words.count(i) for i in Alan.partridge_words()]
-        if sum(wordsFound) != 0:
-            exclaim = "!" * sum(wordsFound)
-            return "Mine's a Pint" + exclaim
-        else:
-            return "Lynn, I've pierced my foot on a spike!!"
+
+def _partridge_words():
+    return ["Dan", "Lynn"]
--- tests/test_partridge.py
+++ tests/test_partridge.py
@@ -1,31 +1,31 @@
-from alan_pytridge.pint import Alan
+from alan_pytridge.pint import getPhrase
 
 
 def test_nothing_found_in_empty_list():
     words = []
-    phrase = Alan().getPhrase(words)
+    phrase = getPhrase(words)
     assert phrase == "Lynn, I've pierced my foot on a spike!!"
 
 
 def test_nothing_found_in_list_with_no_partridge_words():
     words = ["foo", "bar", "baz"]
-    phrase = Alan().getPhrase(words)
+    phrase = getPhrase(words)
     assert phrase == "Lynn, I've pierced my foot on a spike!!"
 
 
 def test_finds_a_single_partridge_words():
     words = ["foo", "Dan", "baz"]
-    phrase = Alan().getPhrase(words)
+    phrase = getPhrase(words)
     assert phrase == "Mine's a Pint!"
 
 
 def test_finds_multiple_partridge_words():
     words = ["foo", "Dan", "Lynn", "baz"]
-    phrase = Alan().getPhrase(words)
+    phrase = getPhrase(words)
     assert phrase == "Mine's a Pint!!"
 
 
 def test_finds_multiple_partridge_words2():
     words = ["Dan", "foo", "Dan", "Lynn", "baz"]
-    phrase = Alan().getPhrase(words)
+    phrase = getPhrase(words)
     assert phrase == "Mine's a Pint!!!"
